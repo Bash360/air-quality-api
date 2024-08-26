f55c7eb3fb20457fd52931fdac05c28ced288a5d
bash <beejayphil@gmail.com>
2024-08-26 21:37:53 +0100

    test working properly

--- __tests__/air-quality/airQuality.test.ts
+++ __tests__/air-quality/airQuality.test.ts
@@ -1,11 +1,19 @@
 import request from "supertest";
 import express from "express";
 import airQualityRouter from "../../src/modules/airQuality/airQuality.route";
+import AppDataSource  from "../../src/config/dataSource";
 
 const app = express();
 app.use(express.json());
 app.use("/api/airquality", airQualityRouter);
 
+beforeAll(async () => {
+  await AppDataSource.initialize();
+});
+
+afterAll(async () => {
+  await AppDataSource.destroy();
+});
 describe("Get air quality by latitude, longitude /api/airquality", () => {
   it("should return 400 if latitude or longitude is missing", async () => {
     const res = await request(app).get("/api/airquality");
@@ -32,5 +40,15 @@ describe("Get air quality by latitude, longitude /api/airquality", () => {
     expect(pollution).toHaveProperty("mainus");
     expect(pollution).toHaveProperty("aqicn");
     expect(pollution).toHaveProperty("maincn");
+  });
+});
+
+describe("Get Date and time where paris is the most polluted /api/airquality/most-polluted ", () => {
+  it("should return 200", async () => {
+    const res = await request(app).get("/api/airquality/most-polluted");
+    const { Result } = res.body;
+    expect(res.status).toBe(200);
+    expect(Result).toHaveProperty("dateTime");
+    expect(Result).toHaveProperty("aqius");
   });
 });
