c539110715a2b22601798c4c6e1746afcb32fd20
bash <beejayphil@gmail.com>
2024-08-26 17:31:27 +0100

    added swagger

--- src/modules/airQuality/airQuality.route.ts
+++ src/modules/airQuality/airQuality.route.ts
@@ -5,6 +5,30 @@ import { validateCoordinatesQuery } from '../../middleware/airQuality.middleware
 const airQualityRouter = Router();
 const airQualityController = new AirQualityController();
 
+/**
+ * @swagger
+ * /airquality:
+ *   get:
+ *     summary: Get air quality data by coordinates
+ *     parameters:
+ *       - in: query
+ *         name: latitude
+ *         required: true
+ *         schema:
+ *           type: number
+ *         description: Latitude of the location
+ *       - in: query
+ *         name: longitude
+ *         required: true
+ *         schema:
+ *           type: number
+ *         description: Longitude of the location
+ *     responses:
+ *       200:
+ *         description: Successful operation
+ *       400:
+ *         description: Invalid query parameters
+ */
 airQualityRouter.get("/", validateCoordinatesQuery, airQualityController.getAirQuality);
 
 export default airQualityRouter;
